14959073b226fdbc0874466b37983ffbd6a11676
Javorov1103 <k.javorov@gmail.com>
2018-09-03 08:18:42 +0300

    Added a global Exception Handler in the API

--- DatingApp.API/Controllers/AuthController.cs
+++ DatingApp.API/Controllers/AuthController.cs
@@ -46,6 +46,8 @@ namespace DatingApp.API.Controllers
 
         public async Task<IActionResult> Login(UserForLoginDto userForLoginDto)
         {
+            throw new Exception("Comuter says no!");
+
             var userFromRepo = await _repo
             .Login(userForLoginDto.Username.ToLower(), userForLoginDto.Password);
 
