935bd69ab4a10f8a4e807ecdb057f473c454e8e7
Javorov1103 <k.javorov@gmail.com>
2019-05-09 20:59:31 +0300

    Added register component  html

--- DatingApp-SPA/src/app/members/photo-editor/photo-editor.component.ts
+++ DatingApp-SPA/src/app/members/photo-editor/photo-editor.component.ts
@@ -1,6 +1,8 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { FileUploader } from 'ng2-file-upload';
 import { Photo } from '../../_models/photo';
+import { environment } from '../../../environments/environment';
+import { AuthService } from '../../_services/auth.service';
 
 @Component({
   selector: 'app-photo-editor',
@@ -10,18 +12,31 @@ import { Photo } from '../../_models/photo';
 export class PhotoEditorComponent implements OnInit {
   @Input() photos: Photo[];
 
- uploader: FileUploader = new FileUploader({url: URL});
+ uploader: FileUploader;
  hasBaseDropZoneOver = false;
- hasAnotherDropZoneOver = false;
+ baseUrl = environment.apiUrl;
 
 
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
   ngOnInit() {
+    this.initializeUploader();
   }
 
  fileOverBase(e: any): void {
     this.hasBaseDropZoneOver = e;
+  }
+
+  initializeUploader() {
+    this.uploader = new FileUploader({
+      url: this.baseUrl +  'users/' + this.authService.decodedToken.nameid + '/photos',
+      authToken: 'Bearer ' + localStorage.getItem('token'),
+      isHTML5: true,
+      allowedFileType: ['image'],
+      removeAfterUpload: true,
+      autoUpload: false,
+      maxFileSize: 10 * 1024 * 1024
+    });
   }
 
 }
