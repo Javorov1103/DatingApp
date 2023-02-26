d2186409c91880052f3be0dac7df5e9a819370ad
Kalin Javorov <k.javorov@gmail.com>
2023-02-26 22:14:56 +0200

    transfer

--- API/Models/DB/User.cs
+++ API/Models/DB/User.cs
@@ -6,6 +6,8 @@
         public string Username { get; set; }
         public byte[] PasswordHash { get; set; }
         public byte[] PasswordSalt { get; set; }
+
+        public string Password { get; set; }
         public string Gender { get; set; }
         public DateTime DateOfBirth { get; set; }
         public string KnownAs { get; set; }
