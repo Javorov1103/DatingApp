645b0579acd3b4ff29d632a90cab02e10c3ec5dd
Kalin Javorov <k.javorov@gmail.com>
2023-03-15 19:15:59 +0200

    added conn string

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -9,11 +9,19 @@ namespace API.Controllers
     public class UsersController : ControllerBase
     {
         [HttpGet]
+        [Route("")]
         public IEnumerable<User> Get()
         {
             //Return a list of users.
             //Here we have to have a service that will retrieve data from DB
             return new List<User>() { new User() { Id = 1, Username = "Kalin", DateOfBirth = new DateTime(1991, 03, 11) } };
+        }
+
+        [HttpGet]
+        [Route("{id:int}")]
+        public User GetById(int id)
+        {
+            return new User() { Id = 1, Username = "Kalin" };
         }
 
         [HttpPost]
